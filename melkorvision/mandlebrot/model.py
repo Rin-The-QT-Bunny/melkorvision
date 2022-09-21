7189b229d84d7975904181bf0390cfc206454b19
Melkor <rintfd@163.com>
2022-09-22 06:33:32 +0800

    Update model.py

--- melkorvision/mandlebrot/model.py
+++ melkorvision/mandlebrot/model.py
@@ -1,10 +1,13 @@
 import torch
 import torch.nn as nn
 
-import torch_geometric
-import torch_scatter 
-import torch_sparse
+from torch_geometric.nn import max_pool
+from torch_geometric.utils import add_self_loops
+from torch_scatter import scatter_mean
+from torch_sparse import coalesce
 
-class AffinityLayer(nn.Module):
+from abc import ABC, abstractmethod
+
+class AffinityAggregation(nn.Module):
     def __init__(self):
         super().__init__()
