6a9c4fee47e4f8b10e253f452c51136e38316aff
Melkor <rintfd@163.com>
2022-09-22 06:30:00 +0800

    update

---
+++ melkorvision/mandlebrot/model.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+
+import torch_geometric
+import torch_scatter 
+import torch_sparse
--- namomo.py
+++
@@ -1,5 +0,0 @@
-from scapy.all import *
-import time
-
-for i in range(1000):
-    sendp(Ether(dst="ff:ff:ff:ff:ff:ff"))
