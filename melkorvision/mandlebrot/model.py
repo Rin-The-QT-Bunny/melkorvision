ca881d46235af954dc95111bcf0d84a1b0b3ac74
Melkor <rintfd@163.com>
2022-09-22 06:42:11 +0800

    Update model.py

--- melkorvision/mandlebrot/model.py
+++ melkorvision/mandlebrot/model.py
@@ -1,3 +1,4 @@
+
 import torch
 import torch.nn as nn
 
@@ -13,3 +14,16 @@ device = "cuda:0" if torch.cuda.is_available else "cpu"
 class AffinityAggregation(nn.Module):
     def __init__(self):
         super().__init__()
+
+    @abstractmethod
+    def affinities_and_thresholds(self,x,row,col):
+        pass
+
+    # Filter edges index based on method's affinity thresholding
+    # and coarse graph to produce next level's nodes
+    def forward(self,x,edge_index,batch,device=device):
+        row, col = edge_index
+
+        # Collect affinities/threholds to filter edges
+        affinities,threshold,losses = self.affinities_and_thresholds(x,row,col)
+        filtered_edges_index = edge_index[:,affinities>threshold]
