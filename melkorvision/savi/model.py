3233a197bb003a43cc64778a7c8d9de0776ddb03
Melkor <rintfd@163.com>
2022-09-22 13:38:11 +0800

    Update model.py

--- melkorvision/savi/model.py
+++ melkorvision/savi/model.py
@@ -6,9 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class SlotModel(nn.Module):
-    def __init__(self):
+class Encoder(nn.Module):
+    def __init__(self,input_nc=3,z_dim=64,bottom=False):
         super().__init__()
+        self.bottom = bottom
 
-    def forward(self,x):
-        return x
+        if self.bottom:
+            self.enc_down_0 = nn.Sequential([
+                nn.Conv2d(input_nc,z_dim,3,stride=1,padding=1),
+                nn.ReLU(True)])
+        self.enc_down_1 = nn.Sequential(nn.Conv2d(z_dim if bottom else input_nc+4, z_dim, 3, stride=2 if bottom else 1, padding=1),
+                                        nn.ReLU(True))
+        self.enc_down_2 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=2, padding=1),
+                                        nn.ReLU(True))
+        self.enc_down_3 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=2, padding=1),
+                                        nn.ReLU(True))
+        self.enc_up_3 = nn.Sequential(nn.Conv2d(z_dim, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True),
+                                      nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False))
+        self.enc_up_2 = nn.Sequential(nn.Conv2d(z_dim*2, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True),
+                                      nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False))
+        self.enc_up_1 = nn.Sequential(nn.Conv2d(z_dim * 2, z_dim, 3, stride=1, padding=1),
+                                      nn.ReLU(True))
