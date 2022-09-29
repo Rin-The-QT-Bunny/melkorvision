52806e990e23ae4ae666507190f3914d2a201a63
Melkor <rintfd@163.com>
2022-09-29 13:57:44 +0800

    Update example.py

--- example.py
+++ example.py
@@ -3,4 +3,10 @@ import torch.nn as nn
 
 from melkorvision.projection import *
 
+class ObjectCentricNeRF(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,x):
+        return x
 
